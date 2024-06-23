915b7ee6a6f51682f8717c5b1dd4edd313edd89d
dxnielemotta <danielemottarocha@gmail.com>
2024-06-23 10:12:47 -0300

    criando classe ContaCorrente

---
+++ conta/contaCorrente.ts
@@ -0,0 +1,28 @@
+import { Cliente } from "../cliente/cliente";
+import { Conta } from "./conta";
+import { ContaInterface } from "./contaInterface";
+
+export class ContaCorrente extends Conta {
+  constructor(numeroConta: string, saldo: number, cliente: Cliente) {
+    super(numeroConta, saldo, cliente);
+  }
+
+  sacar(valor: number): void {
+    if (this.saldo + 100 < valor) {
+      console.log("Limite de cheque especial insuficiente");
+      return;
+    }
+
+    this.saldo -= valor;
+  }
+
+  transferir(valor: number, contaDestino: ContaInterface): void {
+    if (this.saldo + 100 < valor) {
+      console.log("Limite de cheque especial insuficiente");
+      return;
+    }
+
+    this.saldo -= valor;
+    contaDestino.saldo += valor;
+  }
+}
